b6f9eb2035678898c38979e1a7696b7711aa9608
Benjamin Carl <benjamin.carl@wunderbon.io>
2019-11-07 20:03:11 +0100

    added new improved error handling

--- .circleci/validate-json-data.sh
+++ .circleci/validate-json-data.sh
@@ -10,7 +10,13 @@ for file in ${TARGETPATH}; do
 
   if [ -f "$DATAFILE" ]; then
     echo -e "\e[0mValidating: \e[5m$file\e[0m"
-    ./node_modules/.bin/ajv validate -s $file -d $DATAFILE
+    res=$(./node_modules/.bin/ajv validate -s $file -d $DATAFILE 1>&2)
+
+    if [ "$?" = "1" ]; then
+      # exit with error - important for ci system
+      exit 1
+    fi
+
   else
     echo -e "\e[0m\e[31mERROR: Required data file \"$DATAFILE\" does not exist!\e[0m"
   fi
