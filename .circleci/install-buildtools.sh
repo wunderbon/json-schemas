f716bff5d29ea6e272b41932ffe5d99349604700
Benjamin Carl <benjamin.carl@wunderbon.io>
2019-11-08 23:32:48 +0100

    added buildtools installer // updated config

---
+++ .circleci/install-buildtools.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install tools
+apk update && \
+  apk add --no-cache \
+  openssl \
+  openssl-dev \
+  libffi-dev \
+  build-base \
+  py2-pip \
+  python2-dev \
+  git \
+  openssh-client \
+  tar \
+  curl \
+  gettext
+
+# Upgrade pip
+pip install --upgrade pip
+
+if [ "$?" = "0" ]; then
+  # exit with success - important for ci system
+  exit 0
+else
+	echo "Error installing build-tools!" 1>&2
+	# exit with error - important for ci system
+	exit 1
+fi
