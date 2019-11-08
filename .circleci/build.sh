506aa49c3bec23de5c725f844f80465939569ba2
Benjamin Carl <benjamin.carl@wunderbon.io>
2019-11-08 16:43:52 +0100

    added identifier schema // build adjustments

--- .circleci/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build the JSON-Schemas
-./.circleci/dereference-json-schemas.sh && \
-./.circleci/validate-json-schemas.sh && \
-./.circleci/validate-json-data.sh
-
-# Now -> Create Fake data
