ff52c1ac597393234ce0e7a2c80889d7430cab1d
Benjamin Carl <benjamin.carl@wunderbon.io>
2019-11-09 02:23:17 +0100

    added dts bundle generator for publishing ts implementation and typings

--- .circleci/create-typescript-declarations.sh
+++ .circleci/create-typescript-declarations.sh
@@ -10,6 +10,13 @@ for file in ${TARGETPATH}; do
   OUTPUTFILENAME=${OUTPUTFILENAME/.schema.json/.ts}
   mkdir -p "`dirname $OUTPUTFILENAME`/"
   ./node_modules/.bin/quicktype --src-lang schema --lang ts --acronym-style pascal ${file} -o ${OUTPUTFILENAME}
+
+  DTSFILENAME="`basename $OUTPUTFILENAME`"
+  DTSFILENAME=${DTSFILENAME/.ts/}
+  DTSFILENAME=.tmp/types/${DTSFILENAME}/${DTSFILENAME}.d.ts
+  mkdir -p "`dirname $DTSFILENAME`/"
+  ./node_modules/.bin/dts-bundle-generator -o ${DTSFILENAME} ${OUTPUTFILENAME}
+
   if [ "$?" = "1" ]; then
     echo "Error while creating typescript declarations!" 1>&2
 	  # exit with error - important for ci system
