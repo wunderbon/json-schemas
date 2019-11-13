3e56a1732d6378da7d718556327ae7fa894ae8ac
Benjamin Carl <benjamin.carl@wunderbon.io>
2019-11-13 23:36:25 +0100

    added license to generated files

--- .circleci/create-typescript-declarations.sh
+++ .circleci/create-typescript-declarations.sh
@@ -21,6 +21,9 @@ for file in ${PATH_SOURCE_FILES}; do
   mkdir -p "`dirname $DTSFILENAME`/"
   ./node_modules/.bin/dts-bundle-generator -o ${DTSFILENAME} ${OUTPUTFILENAME}
 
+  # Add document header to file
+  echo -e "$(cat .circleci/doc-header.tpl)\n\n$(cat ${OUTPUTFILENAME})" > ${OUTPUTFILENAME}
+
   if [ "$?" = "1" ]; then
     echo "Error while creating typescript declarations!" 1>&2
 	  # exit with error - important for ci system
