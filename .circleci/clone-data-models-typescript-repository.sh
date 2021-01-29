684204e95fdb5252eda8ff6dce0bf01df42bdc24
CircleCI <benjamin.carl@wunderbon.io>
2021-01-29 01:57:17 +0100

    fixed bug in build publish step

---
+++ .circleci/clone-data-models-typescript-repository.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Environment configuration
+# Repository access data (configured at https://circleci.com/bb/organizations/wunderbon/settings#contexts)
+TARGETPATH="/tmp"
+GIT_REPOSITORY_BRANCH="master"
+GIT_REPOSITORY_URL="https://${GIT_REPOSITORY_USERNAME}@bitbucket.org/wunderbon/data-models-typescript.git"
+
+# What we do
+echo "Cloning typescript declarations repository from \"${GIT_REPOSITORY_URL}\""
+
+# Do it
+git clone -b ${GIT_REPOSITORY_BRANCH} ${GIT_REPOSITORY_URL} ${TARGETPATH}/data-models-typescript 2>&1
+
+# Return status
+if [ "$?" = "0" ]; then
+  # exit with success - important for ci system
+  exit 0
+else
+	echo "Error while cloning repository!" 1>&2
+	# exit with error - important for ci system
+	exit 1
+fi
