ead15bee9a51675aff14868e3a79a6592a1d7674
CircleCI <benjamin.carl@wunderbon.io>
2021-01-30 23:31:11 +0100

    test releasing single files

--- .circleci/create-typescript-declarations.sh
+++ .circleci/create-typescript-declarations.sh
@@ -9,11 +9,28 @@ echo "Creating typescript declarations from JSON-Schemas from \"${PATH_SOURCE_FI
 # Create directory first
 mkdir -p ./build/ts
 
-# One call due to namespace trouble on single file conversion
-./node_modules/.bin/quicktype --src-lang schema --lang ts --acronym-style pascal --src ./build/schema --out ./build/ts/index.ts
+# Create an index.ts file
+touch ./build/ts/index.ts
 
-# Add document header to file
-echo -e "$(cat ./.circleci/doc-header-unlicensed.tpl)\n\n$(cat ./build/ts/index.ts)" > ./build/ts/index.ts
+# Do it
+shopt -s globstar
+for file in ${PATH_SOURCE_FILES}; do
+  # New filename and path for dereferenced schema
+  OUTPUTFILENAME="`basename $file`"
+  TSMODELNAME=${OUTPUTFILENAME/.schema.json/}
+  OUTPUTFILENAME=build/ts/${OUTPUTFILENAME/.schema.json/.ts}
+  mkdir -p "`dirname $OUTPUTFILENAME`/"
+  ./node_modules/.bin/quicktype --src-lang schema --lang ts --acronym-style pascal ${file} -o ${OUTPUTFILENAME}
+
+  # Add document header to file
+  echo -e "$(cat .circleci/doc-header-unlicensed.tpl)\n\n$(cat ${OUTPUTFILENAME})" > ${OUTPUTFILENAME}
+
+  if [ "$?" = "1" ]; then
+    echo "Error while creating typescript declarations!" 1>&2
+	  # exit with error - important for ci system
+	  exit 1
+  fi
+done
 
 if [ "$?" = "0" ]; then
   # exit with success - important for ci system
